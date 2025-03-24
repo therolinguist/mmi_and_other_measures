67db5aeecc57db4d94fdb6edc91ff79d08690a0d
therolinguist <nyrestein@gmail.com>
2025-03-24 19:51:17 +0300

    Add files via upload

---
+++ application.py
@@ -0,0 +1,151 @@
+import sys,math,statistics,re
+import pandas as pd
+import mi_family
+from mi_family import nesting_function
+from mi_family import mmi
+from mi_family import mi3
+from mi_family import mi3_exp
+from mi_family import pmi
+from mi_family import treshold
+f=re.compile('\([A-Z\|\s]+\)')
+g=re.compile('[A-Z]+')
+last=re.compile('[а-я][а-яё\|\s]+')
+file = open('/Users/lana/Documents/connectors/connectors.csv','r')
+source=file.readlines()
+connectors_dic={}
+merged=[]
+merged2=[]
+merged3=[]
+merged4=[]
+merged5=[]
+for line in source:
+    l=line.split(',')   
+    connectors_dic[l[0]]=l[1:]
+patterns_dic={}
+cnt_words=[]
+pairs_total=[]
+for item in connectors_dic.keys():
+    wws=item.split(' ')
+    cnt_words=cnt_words+wws
+    clean_pattern=[]
+    pattern=connectors_dic[item][1]
+    for i in re.findall(f,pattern):
+        h=list(set(re.findall(g,i)))
+        h.sort()
+        clean_pattern.append(h)
+    patterns_dic[item]=clean_pattern
+    for w in wws:
+        if 0 <= wws.index(w) < len(clean_pattern):
+            p=(w,clean_pattern[wws.index(w)])
+            pairs_total.append(p)
+final_cnt_words=list(set(cnt_words))
+final_cnt_words.sort()
+for abc in final_cnt_words:
+    abc_patterns=[]
+    for t in pairs_total:
+        if t[0] == abc:
+           abc_patterns.append(str(t[1]))
+    abcp=list(set(abc_patterns))
+file2 = open('/Users/lana/Documents/connectors/embeddings.txt','r',encoding='UTF-8')
+content = file2.readlines()
+embeddings_dictionary={}
+for line in content:
+    s=line.split('[')
+    key=s[0][2:-3]
+    value=last.findall(s[1])
+    embeddings_dictionary[key]=value
+result=[]
+question=[]
+mmi_2=[]
+mmi_3=[]
+mmi_4=[]
+mmi_5=[]
+mi3_2=[]
+mi3_3=[]
+mi3_4=[]
+mi3_5=[]
+mi3_exp_2=[]
+mi3_exp_3=[]
+mi3_exp_4=[]
+mi3_exp_5=[]
+pmi_2=[]
+pmi_3=[]
+pmi_4=[]
+pmi_5=[]
+for item in connectors_dic.keys():
+    if connectors_dic[item][0]=='2':
+        if connectors_dic[item][7]=='единый':
+            mmi_2.append(mmi(embeddings_dictionary,connectors_dic,item))
+            mi3_2.append(mi3(embeddings_dictionary,connectors_dic,item,374449975))
+            mi3_exp_2.append(mi3_exp(embeddings_dictionary,connectors_dic,item,374449975))
+            pmi_2.append(pmi(embeddings_dictionary,connectors_dic,item,374449975))
+            result.append({'connective': item,'mmi': mmi(embeddings_dictionary,connectors_dic,item), \
+                              'mi3': mi3_exp(embeddings_dictionary,connectors_dic,item,374449975), \
+                              'pmi': pmi(embeddings_dictionary,connectors_dic,item,374449975)})
+        if connectors_dic[item][7]=='вопрос':
+            question.append({'connective': item,'mmi': mmi(embeddings_dictionary,connectors_dic,item), \
+                              'mi3': mi3_exp(embeddings_dictionary,connectors_dic,item,374449975), \
+                              'pmi': pmi(embeddings_dictionary,connectors_dic,item,374449975)})
+    if connectors_dic[item][0]=='3':
+        if connectors_dic[item][8]=='единый':
+            mmi_3.append(mmi(embeddings_dictionary,connectors_dic,item))
+            mi3_3.append(mi3(embeddings_dictionary,connectors_dic,item,374449975))
+            mi3_exp_3.append(mi3_exp(embeddings_dictionary,connectors_dic,item,374449975))
+            pmi_3.append(pmi(embeddings_dictionary,connectors_dic,item,374449975))
+            result.append({'connective': item,'mmi': mmi(embeddings_dictionary,connectors_dic,item), \
+                              'mi3': mi3_exp(embeddings_dictionary,connectors_dic,item,374449975), \
+                              'pmi': pmi(embeddings_dictionary,connectors_dic,item,374449975)})
+        if connectors_dic[item][8]=='вопрос':
+            question.append({'connective': item,'mmi': mmi(embeddings_dictionary,connectors_dic,item), \
+                              'mi3': mi3_exp(embeddings_dictionary,connectors_dic,item,374449975), \
+                              'pmi': pmi(embeddings_dictionary,connectors_dic,item,374449975)})
+    if connectors_dic[item][0]=='4':
+        if connectors_dic[item][9]=='единый':
+            mmi_4.append(mmi(embeddings_dictionary,connectors_dic,item))
+            mi3_4.append(mi3(embeddings_dictionary,connectors_dic,item,374449975))
+            mi3_exp_4.append(mi3_exp(embeddings_dictionary,connectors_dic,item,374449975))
+            pmi_4.append(pmi(embeddings_dictionary,connectors_dic,item,374449975))
+            result.append({'connective': item,'mmi': mmi(embeddings_dictionary,connectors_dic,item), \
+                              'mi3': mi3_exp(embeddings_dictionary,connectors_dic,item,374449975), \
+                              'pmi': pmi(embeddings_dictionary,connectors_dic,item,374449975)})
+        if connectors_dic[item][9]=='вопрос':
+            question.append({'connective': item,'mmi': mmi(embeddings_dictionary,connectors_dic,item), \
+                              'mi3': mi3_exp(embeddings_dictionary,connectors_dic,item,374449975), \
+                              'pmi': pmi(embeddings_dictionary,connectors_dic,item,374449975)})
+    if connectors_dic[item][0]=='5':
+        if connectors_dic[item][10]=='единый' or connectors_dic[item][10]=='единый\n':
+            mmi_5.append(mmi(embeddings_dictionary,connectors_dic,item))
+            mi3_5.append(mi3(embeddings_dictionary,connectors_dic,item,374449975))
+            mi3_exp_5.append(mi3_exp(embeddings_dictionary,connectors_dic,item,374449975))
+            pmi_5.append(pmi(embeddings_dictionary,connectors_dic,item,374449975))
+            result.append({'connective': item,'mmi': mmi(embeddings_dictionary,connectors_dic,item), \
+                              'mi3': mi3_exp(embeddings_dictionary,connectors_dic,item,374449975), \
+                              'pmi': pmi(embeddings_dictionary,connectors_dic,item,374449975)})
+        if connectors_dic[item][10]=='вопрос':
+            question.append({'connective': item,'mmi': mmi(embeddings_dictionary,connectors_dic,item), \
+                              'mi3': mi3_exp(embeddings_dictionary,connectors_dic,item,374449975), \
+                              'pmi': pmi(embeddings_dictionary,connectors_dic,item,374449975)})
+print(len(result))           
+print(len(question))
+treshold_dic={}
+treshold_dic['mmi_2']=treshold(mmi_2)
+treshold_dic['mmi_3']=treshold(mmi_3)
+treshold_dic['mmi_4']=treshold(mmi_4)
+treshold_dic['mmi_5']=treshold(mmi_5)
+treshold_dic['mi3_2']=treshold(mi3_2)
+treshold_dic['mi3_3']=treshold(mi3_3)
+treshold_dic['mi3_4']=treshold(mi3_4)
+treshold_dic['mi3_5']=treshold(mi3_5)
+treshold_dic['mi3_exp_2']=treshold(mi3_exp_2)
+treshold_dic['mi3_exp_3']=treshold(mi3_exp_3)
+treshold_dic['mi3_exp_4']=treshold(mi3_exp_4)
+treshold_dic['mi3_exp_5']=treshold(mi3_exp_5)
+treshold_dic['pmi_2']=treshold(pmi_2)
+treshold_dic['pmi_3']=treshold(pmi_3)
+treshold_dic['pmi_4']=treshold(pmi_4)
+treshold_dic['pmi_5']=treshold(pmi_5)
+for i in treshold_dic.items():
+    print (i)
+pd.DataFrame(result).to_csv('result.csv',index=False)
+pd.DataFrame(question).to_csv('connectives_in_question.csv',index=False)
+sys.exit()    
---
+++ mi_family.py
@@ -0,0 +1,105 @@
+import sys,math,statistics
+def nesting_function(embeddings_dictionary,connectors_dic,a): # проверка выражения на вложенность и поправка наблюдаемой абсолютной частоты коннектора:
+    # если короткое выражение входит в состав более длинного, то надо вычесть из абсолютной частотности первого абсолютную частотность второго
+    r=connectors_dic[a]
+    f_obs=int(r[3]) # значение наблюдаемой абсолютной частоты коннектора в таблице
+    if a in embeddings_dictionary.keys(): # embeddings_dictionary - Словарь, составляемый по заранее подготовленному текстовому файлу
+        minus=0
+        for smth in embeddings_dictionary[a]:
+            ff_obs=int(connectors_dic[smth][3])
+            minus=minus+ff_obs
+        f_obs=int(r[3])-minus
+    else:
+        f_obs=int(r[3])
+    return f_obs
+def mmi(embeddings_dictionary,connectors_dic,a): # embeddings_dictionary - Словарь, составляемый по заранее подготовленному текстовому файлу
+    r=connectors_dic[a] # connectors_dic - cловарь, составляемый по таблице данных
+    f_obs=nesting_function(embeddings_dictionary,connectors_dic,a)
+    if r[0]=='2':
+        mmi=math.log(f_obs / ((int(r[4]) * int(r[5]))/int(r[6])**2))
+    if r[0]=='3':
+        mmi=math.log(f_obs / ((int(r[4]) * int(r[5]) * int(r[6]))/int(r[7])**3))
+    if r[0]=='4':
+        mmi=math.log(f_obs / ((int(r[4]) * int(r[5]) * int(r[6]) * int(r[7]))/int(r[8])**4))
+    if r[0]=='5':
+        mmi=math.log(f_obs / ((int(r[4]) * int(r[5]) * int(r[6]) * int(r[7]) * int(r[8]))/int(r[9])**5))
+    return round(mmi,5)
+
+def mi3(embeddings_dictionary,connectors_dic,a,N):# формула "MI в кубе", используемая в НКРЯ для ранжирования
+    # результатов при поиске двухсловных коллокаций. Отличается от формулы, предложенной в диссертации
+    # Daille, Béatrice (1994). Approche mixte pour l’extraction automatique de terminologie : statistiques lexicales et filtres linguistiques. Ph.D. thesis, Université Paris 7.
+    # тем, что используется натуральный логарифм, а не логарифм по основанию 2. 
+    r=connectors_dic[a]
+    f_obs=nesting_function(embeddings_dictionary,connectors_dic,a)
+    f=f_obs**3
+    if r[0]=='2':
+        mi3=math.log(f / ((int(r[4]) * int(r[5]))/N))
+    if r[0]=='3':
+        mi3=math.log(f / ((int(r[4]) * int(r[5]) * int(r[6]))/N))
+    if r[0]=='4':
+        mi3=math.log(f / ((int(r[4]) * int(r[5]) * int(r[6]) * int(r[7]))/N))
+    if r[0]=='5':
+        mi3=math.log(f / ((int(r[4]) * int(r[5]) * int(r[6]) * int(r[7]) * int(r[8]))/N))
+    return round(mi3,5)
+
+def mi3_exp(embeddings_dictionary,connectors_dic,a,N): #Вариант предыдущей формулы, адаптированной для словосочетаний разной длины
+    # по той же логике, что и pmi 
+    r=connectors_dic[a]
+    f_obs=nesting_function(embeddings_dictionary,connectors_dic,a)
+    f=f_obs**3
+    if r[0]=='2':
+        mi3=math.log(f / ((int(r[4]) * int(r[5]))/N))
+    if r[0]=='3':
+        mi3=math.log(f / ((int(r[4]) * int(r[5]) * int(r[6]))/N **2))
+    if r[0]=='4':
+        mi3=math.log(f / ((int(r[4]) * int(r[5]) * int(r[6]) * int(r[7]))/N**3))
+    if r[0]=='5':
+        mi3=math.log(f / ((int(r[4]) * int(r[5]) * int(r[6]) * int(r[7]) * int(r[8]))/N**4))
+    return round(mi3,5)
+
+def mi3_with_pattern(embeddings_dictionary,connectors_dic,a):
+    r=connectors_dic[a]
+    f_obs=nesting_function(embeddings_dictionary,connectors_dic,a)
+    f=f_obs**3
+    if r[0]=='2':
+        mi3=math.log(f / ((int(r[4]) * int(r[5]))/int(r[6])))
+    if r[0]=='3':
+        mi3=math.log(f / ((int(r[4]) * int(r[5]) * int(r[6]))/int(r[7])))
+    if r[0]=='4':
+        mi3=math.log(f / ((int(r[4]) * int(r[5]) * int(r[6]) * int(r[7]))/int(r[8])))
+    if r[0]=='5':
+        mi3=math.log(f / ((int(r[4]) * int(r[5]) * int(r[6]) * int(r[7]) * int(r[8]))/int(r[9])))
+    return round(mi3,5)
+
+def pmi(embeddings_dictionary,connectors_dic,a,N): # формула для двухсловных выражений изначально предложена в работе
+    # Church K, Hanks P. Word association norms, mutual information, and lexicography. Computational linguistics. 1990;16(1):22-9.
+    # Черч и Хэнкс предпочитают называть ее association ratio.
+    # мы используем адаптацию формулы для словосочетаний разной длины, предложенную в работе
+    # Ramisch C., Villavicencio A., Boitet C. mwetoolkit: A framework for multiword expression identification // Proceedings of the Seventh International Conference on Language Resources and Evaluation (LREC 2010), Valetta, Malta, 2010. P. 662–669.
+    # И вслед за  [Ramisch et al. 2010] называем ее pointwise mutual information,  сокрщенно pmi
+    r=connectors_dic[a]
+    f_obs=nesting_function(embeddings_dictionary,connectors_dic,a)
+    if r[0]=='2':
+        pmi=math.log(f_obs / ((int(r[4]) * int(r[5]))/N),2)
+    if r[0]=='3':
+        pmi=math.log(f_obs / ((int(r[4]) * int(r[5]) * int(r[6]))/N**2),2)
+    if r[0]=='4':
+        pmi=math.log(f_obs / ((int(r[4]) * int(r[5]) * int(r[6]) * int(r[7]))/N**3),2)
+    if r[0]=='5':
+        pmi=math.log(f_obs / ((int(r[4]) * int(r[5]) * int(r[6]) * int(r[7]) * int(r[8]))/N**4),2)    
+    return round(pmi,5)
+
+def treshold(b):
+    tr_len=len(b)
+    #print(tr_len)
+    tr_mean=round(statistics.mean(b),2)
+    #print(round(tr_mean,2))
+    tr_median=round(statistics.median(b),2)
+    #print(round(tr_median,2))
+    tr_stdev=round(statistics.stdev(b),2)
+    #print(round(tr_stdev,2))
+    tr_min = tr_mean-tr_stdev
+    #print(round(tr_min,2))
+    tr_max= tr_mean+tr_stdev
+    #print(round(tr_max,2))
+    return tr_len, tr_mean, tr_median, tr_stdev, tr_min, tr_max     
